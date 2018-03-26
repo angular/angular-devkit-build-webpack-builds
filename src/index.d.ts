2710afc686c7229b70caa88dce32496115eaa184
Angular Builds <circleci@angular.io>
2018-03-26 16:24:44 +0000

    abc96c2 test: add test for app-shell architect builder

--- src/index.d.ts
+++ src/index.d.ts
@@ -5,9 +5,11 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+export * from './app-shell';
 export * from './browser';
 export * from './dev-server';
 export * from './extract-i18n';
 export * from './karma';
 export * from './protractor';
+export * from './server';
 export * from './tslint';
