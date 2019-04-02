0e990d6faf17af369996049eab4824d9a42ba362
Angular Builds <circleci@angular.io>
2019-04-02 12:11:14 +0000

    3d9200a refactor(@angular-devkit/build-angular): use build-webpack stable API module imports

--- src/webpack-dev-server/index.js
+++ src/webpack-dev-server/index.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -6,59 +7,58 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-Object.defineProperty(exports, "__esModule", { value: true });
+const index2_1 = require("@angular-devkit/architect/src/index2");
 const core_1 = require("@angular-devkit/core");
 const rxjs_1 = require("rxjs");
 const operators_1 = require("rxjs/operators");
 const webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
-const webpack_1 = require("../webpack");
-class WebpackDevServerBuilder {
-    constructor(context) {
-        this.context = context;
+const architect_1 = require("../plugins/architect");
+const utils_1 = require("../utils");
+const webpackMerge = require('webpack-merge');
+function runWebpackDevServer(config, context, options = {}) {
+    const createWebpack = options.webpackFactory || (config => rxjs_1.of(webpack(config)));
+    const log = options.logging
+        || ((stats, config) => context.logger.info(stats.toString(config.stats)));
+    config = webpackMerge(config, {
+        plugins: [
+            new architect_1.ArchitectPlugin(context),
+        ],
+    });
+    const devServerConfig = options.devServerConfig || config.devServer || {};
+    if (devServerConfig.stats) {
+        config.stats = devServerConfig.stats;
     }
-    run(builderConfig) {
-        const configPath = core_1.resolve(this.context.workspace.root, core_1.normalize(builderConfig.options.webpackConfig));
-        return this.loadWebpackConfig(core_1.getSystemPath(configPath)).pipe(operators_1.concatMap(config => this.runWebpackDevServer(config)));
-    }
-    loadWebpackConfig(webpackConfigPath) {
-        return rxjs_1.from(Promise.resolve().then(() => require(webpackConfigPath)));
-    }
-    runWebpackDevServer(webpackConfig, devServerCfg, loggingCb = webpack_1.defaultLoggingCb) {
-        return new rxjs_1.Observable(obs => {
-            const devServerConfig = devServerCfg || webpackConfig.devServer || {};
-            devServerConfig.host = devServerConfig.host || 'localhost';
-            if (devServerConfig.port == undefined) {
-                devServerConfig.port = 8080;
-            }
-            if (devServerConfig.stats) {
-                webpackConfig.stats = devServerConfig.stats;
-            }
-            // Disable stats reporting by the devserver, we have our own logger.
-            devServerConfig.stats = false;
-            const webpackCompiler = webpack(webpackConfig);
-            const server = new WebpackDevServer(webpackCompiler, devServerConfig);
-            let result;
-            webpackCompiler.hooks.done.tap('build-webpack', (stats) => {
-                // Log stats.
-                loggingCb(stats, webpackConfig, this.context.logger);
-                obs.next({ success: !stats.hasErrors(), result });
-            });
-            server.listen(devServerConfig.port, devServerConfig.host, function (err) {
-                if (err) {
-                    obs.error(err);
-                }
-                else {
-                    // this is ignored because of ts errors
-                    // that this is overshadowed by it's outer contain
-                    // @ts-ignore;
-                    result = this.address();
-                }
-            });
-            // Teardown logic. Close the server when unsubscribed from.
-            return () => server.close();
+    // Disable stats reporting by the devserver, we have our own logger.
+    devServerConfig.stats = false;
+    return createWebpack(config).pipe(operators_1.switchMap(webpackCompiler => new rxjs_1.Observable(obs => {
+        const server = new WebpackDevServer(webpackCompiler, devServerConfig);
+        let result;
+        webpackCompiler.hooks.done.tap('build-webpack', (stats) => {
+            // Log stats.
+            log(stats, config);
+            obs.next(Object.assign({}, result, { emittedFiles: utils_1.getEmittedFiles(stats.compilation), success: !stats.hasErrors() }));
         });
-    }
+        server.listen(devServerConfig.port === undefined ? 8080 : devServerConfig.port, devServerConfig.host === undefined ? 'localhost' : devServerConfig.host, function (err) {
+            if (err) {
+                obs.error(err);
+            }
+            else {
+                const address = this.address();
+                result = {
+                    success: true,
+                    port: typeof address === 'string' ? 0 : address.port,
+                    family: typeof address === 'string' ? '' : address.family,
+                    address: typeof address === 'string' ? address : address.address,
+                };
+            }
+        });
+        // Teardown logic. Close the server when unsubscribed from.
+        return () => server.close();
+    })));
 }
-exports.WebpackDevServerBuilder = WebpackDevServerBuilder;
-exports.default = WebpackDevServerBuilder;
+exports.runWebpackDevServer = runWebpackDevServer;
+exports.default = index2_1.createBuilder((options, context) => {
+    const configPath = core_1.resolve(core_1.normalize(context.workspaceRoot), core_1.normalize(options.webpackConfig));
+    return rxjs_1.from(Promise.resolve().then(() => require(core_1.getSystemPath(configPath)))).pipe(operators_1.switchMap((config) => runWebpackDevServer(config, context)));
+});
