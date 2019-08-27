93c10c6bf63e22dc2b95653aa264d1bbed401dbf
Angular Builds <circleci@angular.io>
2019-08-27 17:55:13 +0000

    5606a4cc0 fix(@angular/cli): handle promise rejection on analytics install prompt (#15455)

--- src/utils.d.ts
+++ src/utils.d.ts
@@ -10,6 +10,7 @@ export interface EmittedFiles {
     name?: string;
     file: string;
     initial: boolean;
+    asset?: boolean;
     extension: string;
 }
 export declare function getEmittedFiles(compilation: webpack.compilation.Compilation): EmittedFiles[];
